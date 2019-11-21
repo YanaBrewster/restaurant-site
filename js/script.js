d3e9e6ff89113529fbd285b8ae6eea172916cad8
YanaBrewster <yana.brewster@gmail.com>
2019-11-22 09:53:16 +1300

    site upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+})
+
+function showNav(){
+    var menu = document.getElementById("list");
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
