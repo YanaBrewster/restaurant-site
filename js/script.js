d337ad9af10262ede5c052ec8c1b80d914569459
YanaBrewster <yana.brewster@gmail.com>
2020-06-24 11:06:15 +1200

    footer edited and sweet alert added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-
+  Swal.fire({
+		title: 'Please Acknowledge',
+		text: 'This site is for education use only and is not the real site, please see the real Dockside website at www.docksidenz.com. This site was created for a Yoobee Colleges assignment by Yana Brewster',
+		confirmButtonText: 'I understand that this is not the real site'
+	})
 })
 
 function showNav(){
